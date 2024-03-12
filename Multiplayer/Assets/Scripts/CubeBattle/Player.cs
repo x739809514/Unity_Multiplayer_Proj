3b8dfba4e8da6f6cf8fe92e35cd6f786f9689afd
x739809514 <x739809514@sina.com>
2024-03-12 13:19:52 +0000

    different color sync

--- Multiplayer/Assets/Scripts/CubeBattle/Player.cs
+++ Multiplayer/Assets/Scripts/CubeBattle/Player.cs
@@ -1,23 +1,43 @@
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.UI;
 
 
 public class Player : NetworkBehaviour
 {
     public float moevSpeed;
     public float turnSpeed;
+    public Text textId;
     private float horizontal;
     private float vertical;
+    private Color[] colors = { Color.black, Color.white, Color.red };
+
+    // NetworkVariable is used to synchornize variable between client and server
     private NetworkVariable<Vector3> vPos = new NetworkVariable<Vector3>();
     private NetworkVariable<Quaternion> vRot = new NetworkVariable<Quaternion>();
+    private NetworkVariable<int> clientId = new NetworkVariable<int>();
     private Rigidbody rb;
 
-    private void Awake()
+    private void Start()
     {
         rb = this.GetComponent<Rigidbody>();
         if (IsClient && IsOwner)
         {
             transform.position = new Vector3(Random.Range(-5, 5), 3, Random.Range(-5, 5));
+        }
+        textId.text = "id: " + clientId.Value.ToString();
+        Renderer material = this.GetComponent<MeshRenderer>();
+        material.material.color = colors[(int)clientId.Value % colors.Length];
+    }
+
+    // this method will be called when an object is spawned
+    // it will be called before Start()
+    public override void OnNetworkSpawn()
+    {
+        if (this.IsServer)
+        {
+            clientId.Value = (int)this.OwnerClientId;
+            Debug.Log("Server--The new object's id is: " + this.OwnerClientId);
         }
     }
 
